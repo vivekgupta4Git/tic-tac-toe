450e316e29b327aaf5dfc1498229f8f7ff9e1b17
vivekgupta <itguru4all@gmail.com>
2024-07-14 21:09:42 +0530

    Added Tic-tac-toe game

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Board from './components/Board'
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Board> </Board>
+        <StatusBar style="auto" />
     </View>
   );
 }
@@ -16,5 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
